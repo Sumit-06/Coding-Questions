66dc359fb3ceeff3f521b2113e39adc799304230
Sumit Kumar <sumitkumar06122@gmail.com>
2026-07-08 00:00:38 +0530

    Time: 1 ms (99.96%), Space: 77.2 MB (65.78%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -4,12 +4,14 @@ class Solution {
         int maxSum = Integer.MIN_VALUE;
         for(int i =0 ; i<nums.length ; i++){
             currentSum += nums[i];
+            
             if(currentSum > maxSum){
                 maxSum = currentSum;
             }
             if(currentSum < 0){
                 currentSum = 0;
             }
+            
         }
         return maxSum;
     }
